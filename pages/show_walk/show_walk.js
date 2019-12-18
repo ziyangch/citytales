2db9f00401e006cf09eed88911b0a2dd11120119
KaiSun1990 <kai.j.sun@gmail.com>
2019-12-18 14:09:59 +0100

    improved Skeleton for walks

--- pages/show_walk/show_walk.js
+++ pages/show_walk/show_walk.js
@@ -8,11 +8,65 @@ Page({
 
   },
 
+  navigateToUserProfile(e) {
+    console.log(e.currentTarget)
+    let id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: `/pages/userprofile/userprofile?id=${id}`
+    })
+  },
+
+  setWalk: function(id) {
+    let that = this
+    let Walk = new wx.BaaS.TableObject('walk')
+    Walk.get(id.toString()).then(res => {
+      let walk = res.data;
+      that.setData({ walk: walk })
+      that.setWalkStories()
+    }, err => {
+    })
+  },
+
+  setWalkStories: function() {
+    let that = this
+    let storiesIdArr = that.data.walk.stories_id_arr
+    let query = new wx.BaaS.Query()
+    let Story = new wx.BaaS.TableObject('story')
+    query.compare('created_at', '>', 0)
+    query.compare('visible', '=', true)
+    Story.setQuery(query).find().then(res => {
+      let stories = res.data.objects
+
+      let walkStories = stories.filter(function (item) {
+        return (storiesIdArr.includes(item.id))
+      })
+      that.setData({walkStories: walkStories})
+      that.setMarkers(walkStories)
+    })
+  },
+
+  setMarkers: function(walkStories) {
+    let that = this
+    let markers = walkStories.map(walkStory => {
+      return {
+        id: walkStory.id,
+        latitude: walkStory.latitude,
+        longitude: walkStory.longitude,
+        name: walkStory.title,
+        iconPath: 'https://cloud-minapp-32027.cloud.ifanrusercontent.com/1ifOM5c01ybmL4zj.png',
+        width: 40,
+        height: 40
+      }
+    });
+    that.setData({ markers })
+  },
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    let walkId = options.id // Setting walkId from Page properties
+    this.setWalk(walkId) // setting Walk object by search with Walk ID in local page data
+    let user = wx.getStorageSync('user')
   },
 
   /**
