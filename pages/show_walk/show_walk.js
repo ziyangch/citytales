d697217fc5dcb7e7f3b96cd7ec2e50917bb10579
Mi Gao <mimi.gao1991@hotmail.com>
2019-12-19 22:00:00 +0800

    style

--- pages/show_walk/show_walk.js
+++ pages/show_walk/show_walk.js
@@ -8,7 +8,23 @@ Page({
     visible1: false,
     visible2: false,
     btnShimmering: false,
-    scale: 16
+    scale: 16,
+    toView: 'green',
+    scrollTop: 100
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
   },
 
   open1() {
